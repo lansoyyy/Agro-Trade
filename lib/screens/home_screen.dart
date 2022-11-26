4d213aee6d1dac68847c78433a4c5f49ab61605b
Lans12345 <olanalans12345@gmail.com>
2022-11-26 10:55:23 +0800

    added home layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:marketdo/screens/tabs/home_tab.dart';
 import 'package:marketdo/widgets/appbar_widget.dart';
 import 'package:marketdo/widgets/drawer_widget.dart';
-import 'package:marketdo/widgets/text_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,71 +13,15 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 0;
 
+  final tabs = [const HomeTab(), const MessagesTab()];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       drawer: const DrawerWidget(),
       appBar: AppbarWidget('Home'),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextBold(text: 'Categories', fontSize: 16, color: Colors.black),
-            const SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 175,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: ((context, index) {
-                    return Container(
-                      child: Center(
-                        child: TextBold(
-                            text: 'Insert Here',
-                            fontSize: 24,
-                            color: Colors.white),
-                      ),
-                      margin: const EdgeInsets.all(5),
-                      height: 100,
-                      width: 250,
-                      decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(5)),
-                    );
-                  })),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextBold(text: 'Items', fontSize: 16, color: Colors.black),
-            const SizedBox(
-              height: 10,
-            ),
-            Expanded(
-              child: SizedBox(
-                child: GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2),
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Container(
-                          width: 100,
-                          height: 100,
-                          decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(5)),
-                        ),
-                      );
-                    }),
-              ),
-            )
-          ],
-        ),
-      ),
+      body: SafeArea(child: tabs[_currentIndex]),
       bottomNavigationBar: BottomNavigationBar(
         unselectedItemColor: Colors.grey,
         type: BottomNavigationBarType.shifting,
@@ -99,13 +43,6 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               backgroundColor: Colors.blue[400],
               label: 'Messages'),
-          BottomNavigationBarItem(
-              icon: const Icon(
-                Icons.person,
-                color: Colors.white,
-              ),
-              backgroundColor: Colors.red[400],
-              label: 'Profile'),
         ],
         onTap: (index) {
           setState(() {
---
+++ lib/screens/tabs/home_tab.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:marketdo/widgets/text_widget.dart';
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextBold(text: '    Categories', fontSize: 16, color: Colors.black),
+          const SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            height: 175,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: ((context, index) {
+                  return Container(
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 20, left: 10),
+                        child: TextBold(
+                            text: 'Category',
+                            fontSize: 14,
+                            color: Colors.black),
+                      ),
+                    ),
+                    margin: const EdgeInsets.all(5),
+                    height: 100,
+                    width: 250,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5)),
+                  );
+                })),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextBold(text: '    Items', fontSize: 16, color: Colors.black),
+          const SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: SizedBox(
+              child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Container(
+                        padding: const EdgeInsets.all(5),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextBold(
+                                text: 'Item Name',
+                                fontSize: 14,
+                                color: Colors.black),
+                            TextRegular(
+                                text: 'Item Description',
+                                fontSize: 12,
+                                color: Colors.grey),
+                          ],
+                        ),
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5)),
+                      ),
+                    );
+                  }),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MessagesTab extends StatelessWidget {
+  const MessagesTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
