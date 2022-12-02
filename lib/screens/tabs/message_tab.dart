b98a12c3f0aa25f15ad4f7d8137e2e3b578bb7d8
Lans12345 <olanalans12345@gmail.com>
2022-12-02 19:34:04 +0800

    added messaging core page

--- lib/screens/tabs/message_tab.dart
+++ lib/screens/tabs/message_tab.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:marketdo/widgets/text_widget.dart';
 
 class MessageTab extends StatelessWidget {
   const MessageTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return ListView.builder(itemBuilder: ((context, index) {
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+        child: ListTile(
+          leading: const CircleAvatar(
+            minRadius: 25,
+            maxRadius: 25,
+            backgroundImage: AssetImage('assets/images/profile.png'),
+          ),
+          title:
+              TextBold(text: 'Lance Olana', fontSize: 14, color: Colors.black),
+          subtitle:
+              TextRegular(text: '08:45pm', fontSize: 10, color: Colors.grey),
+          tileColor: Colors.white,
+          trailing: IconButton(
+            onPressed: () {
+              //  FirebaseFirestore.instance
+              //                               .collection('offer')
+              //                               .doc(data.docs[index].id)
+              //                               .delete();
+            },
+            icon: const Icon(Icons.delete, color: Colors.red),
+          ),
+        ),
+      );
+    }));
   }
 }
