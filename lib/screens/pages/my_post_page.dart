7debc73e1756a4381607fa4dfbcbbdf9be5bd4dd
Lans12345 <olanalans12345@gmail.com>
2022-12-10 11:57:27 +0800

    wip editable items

--- lib/screens/pages/my_post_page.dart
+++ lib/screens/pages/my_post_page.dart
@@ -1,13 +1,93 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:marketdo/widgets/appbar_widget.dart';
+import 'package:marketdo/widgets/button_widget.dart';
 import 'package:marketdo/widgets/drawer_widget.dart';
-
+import 'package:path/path.dart' as path;
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import '../../widgets/text_widget.dart';
 
-class MyPostPage extends StatelessWidget {
+class MyPostPage extends StatefulWidget {
   const MyPostPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyPostPage> createState() => _MyPostPageState();
+}
+
+class _MyPostPageState extends State<MyPostPage> {
+  late String prodName = '';
+
+  late String prodDesc = '';
+
+  var hasLoaded = false;
+
+  firebase_storage.FirebaseStorage storage =
+      firebase_storage.FirebaseStorage.instance;
+
+  late String fileName = '';
+
+  late File imageFile;
+
+  late String imageURL = '';
+
+  Future<void> uploadPicture(String inputSource) async {
+    final picker = ImagePicker();
+    XFile pickedImage;
+    try {
+      pickedImage = (await picker.pickImage(
+          source: inputSource == 'camera'
+              ? ImageSource.camera
+              : ImageSource.gallery,
+          maxWidth: 1920))!;
+
+      fileName = path.basename(pickedImage.path);
+      imageFile = File(pickedImage.path);
+
+      try {
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) => const Padding(
+            padding: EdgeInsets.only(left: 30, right: 30),
+            child: AlertDialog(
+                title: Text(
+              '         Loading . . .',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Quicksand'),
+            )),
+          ),
+        );
+
+        await firebase_storage.FirebaseStorage.instance
+            .ref('Products/$fileName')
+            .putFile(imageFile);
+        imageURL = await firebase_storage.FirebaseStorage.instance
+            .ref('Products/$fileName')
+            .getDownloadURL();
+
+        setState(() {
+          hasLoaded = true;
+        });
+
+        Navigator.of(context).pop();
+      } on firebase_storage.FirebaseException catch (error) {
+        if (kDebugMode) {
+          print(error);
+        }
+      }
+    } catch (err) {
+      if (kDebugMode) {
+        print(err);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,16 +136,143 @@ class MyPostPage extends StatelessWidget {
                 ], rows: [
                   for (int i = 0; i < data.size; i++)
                     DataRow(cells: [
-                      DataCell(Container(
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          image: DecorationImage(
-                              image: NetworkImage(data.docs[i]['imageURL']),
-                              fit: BoxFit.cover),
+                      DataCell(GestureDetector(
+                        onTap: () {
+                          showModalBottomSheet(
+                              context: context,
+                              builder: (context) {
+                                return Container(
+                                  color: Colors.grey[200],
+                                  height: 500,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      GestureDetector(
+                                        onTap: () {
+                                          uploadPicture('gallery');
+                                        },
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            color: Colors.black,
+                                            image: DecorationImage(
+                                                image: NetworkImage(
+                                                    data.docs[i]['imageURL']),
+                                                fit: BoxFit.cover),
+                                          ),
+                                          margin: const EdgeInsets.all(2.5),
+                                          height: 120,
+                                          width: 100,
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.fromLTRB(
+                                            30, 10, 30, 10),
+                                        child: TextFormField(
+                                          style: const TextStyle(
+                                              color: Colors.black,
+                                              fontFamily: 'QRegular'),
+                                          onChanged: (_input) {
+                                            prodName = _input;
+                                          },
+                                          decoration: InputDecoration(
+                                            fillColor: Colors.white,
+                                            filled: true,
+                                            enabledBorder: OutlineInputBorder(
+                                              borderSide: const BorderSide(
+                                                  width: 1,
+                                                  color: Colors.white),
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                            ),
+                                            focusedBorder: OutlineInputBorder(
+                                              borderSide: const BorderSide(
+                                                  width: 1,
+                                                  color: Colors.black),
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                            ),
+                                            hintText: data.docs[i]['prodName'],
+                                            hintStyle: const TextStyle(
+                                              fontFamily: 'QRegular',
+                                              color: Colors.black,
+                                              fontSize: 12.0,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.fromLTRB(
+                                            30, 10, 30, 10),
+                                        child: TextFormField(
+                                          style: const TextStyle(
+                                              color: Colors.black,
+                                              fontFamily: 'QRegular'),
+                                          onChanged: (_input) {
+                                            prodDesc = _input;
+                                          },
+                                          decoration: InputDecoration(
+                                            fillColor: Colors.white,
+                                            filled: true,
+                                            enabledBorder: OutlineInputBorder(
+                                              borderSide: const BorderSide(
+                                                  width: 1,
+                                                  color: Colors.white),
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                            ),
+                                            focusedBorder: OutlineInputBorder(
+                                              borderSide: const BorderSide(
+                                                  width: 1,
+                                                  color: Colors.black),
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                            ),
+                                            hintText: data.docs[i]['prodDesc'],
+                                            hintStyle: const TextStyle(
+                                              fontFamily: 'QRegular',
+                                              color: Colors.black,
+                                              fontSize: 12.0,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 10,
+                                      ),
+                                      ButtonWidget(
+                                          onPressed: () {
+                                            FirebaseFirestore.instance
+                                                .collection('products')
+                                                .doc(data.docs[i].id)
+                                                .update({
+                                              'prodName': prodName == ''
+                                                  ? data.docs[i]['prodName']
+                                                  : prodName,
+                                              'prodDesc': prodName == ''
+                                                  ? data.docs[i]['prodDesc']
+                                                  : prodDesc,
+                                              'imageURL': prodName == ''
+                                                  ? data.docs[i]['imageURL']
+                                                  : imageURL,
+                                            });
+                                          },
+                                          text: 'Edit Post'),
+                                    ],
+                                  ),
+                                );
+                              });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            image: DecorationImage(
+                                image: NetworkImage(data.docs[i]['imageURL']),
+                                fit: BoxFit.cover),
+                          ),
+                          margin: const EdgeInsets.all(2.5),
+                          height: 70,
+                          width: 50,
                         ),
-                        margin: const EdgeInsets.all(2.5),
-                        height: 70,
-                        width: 50,
                       )),
                       DataCell(
                         TextRegular(
